537f72924ae06b3220125b971eda29b837fee529
Bulat Nasibullin <nasibullinbulat95@gmail.com>
2020-09-19 16:08:43 +0400

    chore(deps): add react-router-dom and home page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import {
+  Switch,
+  BrowserRouter,
+  Route,
+} from 'react-router-dom';
+
+import Home from './pages/Home';
 
 import styles from './App.module.scss';
 
 function App(): JSX.Element {
   return (
-    <div className={styles.title}>Hello, World!</div>
+    <>
+      <div className={styles.title}>Hello, World!</div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Home (): JSX.Element {
+  return (
+    <div>Test!</div>
+  );
+}
+
+export default Home;
